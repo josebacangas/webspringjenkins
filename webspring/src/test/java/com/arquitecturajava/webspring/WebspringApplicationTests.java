d5df40b5dbcfa123341cbc22c6245237e94d06e2
A06P0365\sistemas.madrid <joseba.cangas@altia.es>
2024-11-14 12:36:27 +0100

    pruebas

--- webspring/src/test/java/com/arquitecturajava/webspring/WebspringApplicationTests.java
+++ webspring/src/test/java/com/arquitecturajava/webspring/WebspringApplicationTests.java
@@ -1,8 +1,22 @@
 package com.arquitecturajava.webspring;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.arquitecturajava.webspring.models.Persona;
 
 @SpringBootTest
 class WebspringApplicationTests {
 
+	@Autowired
+	PersonaRepository repositorioPersona;
+	
+	@Test
+	void addPersonaRepositorioTest() {
+		repositorioPersona.add(new Persona("juan"));
+		assertEquals(repositorioPersona.buscarTodos().size(), 2);
+	}
 }
